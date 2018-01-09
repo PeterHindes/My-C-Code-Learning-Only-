8931aef96e942aacba876b3041d6f240a7e22b16
Peter Hindes <ph9214@gmail.com>
2018-01-09 13:28:54 -0700

    lol

--- learning c.c
+++ learning c.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <windows.h>
 
-#define str_y_length 3
-#define str_x_length 5
+#define str_y_length 10
+#define str_x_length 25
 
 int game_board[str_y_length][str_x_length];
 
 int main(int argc, char const *argv[]) {
 
-
+  printf("\e[1;1H\e[2J");
 
   for (size_t i = 0; i < 5; i++) {
     for (size_t x = 0; x < str_x_length; x++) {
@@ -17,16 +19,23 @@ int main(int argc, char const *argv[]) {
     }
   }
 
-
+  while(1){
+    screen_refresh();
+    sleep(1);
+    printf("\e[1;1H\e[2J");
+  }
 
   return 0;
 }
 
+
 int screen_refresh(){
 
-  for (size_t x = 0; x < str_x_length; x++) {
-    for (size_t y = 0; y < str_y_length; y++) {
-      printf("%d\n", game_board[y][x]);
+  for (size_t y = 0; y < str_y_length; y++) {
+    for (size_t x = 0; x < str_x_length; x++) {
+      printf("%d", game_board[y][x]);
     }
+    printf("\n");
   }
+
 }
